dbf50f4249529812d246c097fd185c6e9a76dfc6
muflih1 <muflimuflih306@gmail.com>
2025-03-02 18:59:57 +0530

    login endpoint fixed web

--- web/src/services/LoginApi.ts
+++ web/src/services/LoginApi.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from "../lib/axios";
 
 export function login({
   email,
   password
 }: { email: string; password: string }) {
   const payload = { email, password }
-  return axios.post('http://localhost:3000/api/v1/auth/login', payload, { withCredentials: true })
+  return axios.post('/auth/login', payload, { withCredentials: true })
 }
