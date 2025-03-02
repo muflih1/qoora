15688b7b7f19e4cff9a783508745e9a624755ca1
muflih1 <muflimuflih306@gmail.com>
2025-03-02 17:39:26 +0530

    removed nodemailer and googleapis to avoid heap overflow

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -4,41 +4,9 @@ import { snowflake } from "../lib/snowflake.js";
 import bcrypt from "bcryptjs"
 import { createSession, createSessionCookie } from "../lib/session.js";
 import { catchAsync } from "../utils/catchAsync.js";
-import { sendVerificationEmail } from "../utils/mailer.js";
-import { db } from "../config/db.js";
 import BadRequestError from "../errors/BadRequestError.js";
 
 const userRepository = UserRepository.getInstance()
-
-export const emailAvailableHandler = catchAsync(async (req, res) => {
-  const { email } = req.query
-  const user = await userRepository.getOneBy({ email: email as string })
-  return res.status(200).json({ status: 'ok', available: user == null ? true : false })
-})
-
-export const sendEmailConfirmationEmailHandler = catchAsync(async (req, res) => {
-  const { email } = req.body
-  const verificationCode = Math.floor(999999 * Math.random())
-  await db.query('INSERT INTO email_verification_codes (email, verification_code) VALUES ($1, $2)', [email, verificationCode])
-  sendVerificationEmail(email, verificationCode.toString())
-  return res.status(200).json({ status: 'ok' })
-})
-
-export const verfyEmailConfirmationCodeHandler = catchAsync(async (req, res) => {
-  const { verification_code, email } = req.body
-  const [verificationCode] = await db.query<{ email: string; verification_code: number; expires_at: string }>(`
-    SELECT * FROM email_verification_codes WHERE email = $1 AND verification_code = $2
-  `, [email, verification_code])
-  if (!verificationCode) {
-    throw new BadRequestError('The code you entered is wrong. Please try again.')
-  }
-  const isExpired = new Date(verificationCode.expires_at).getTime() <= Date.now()
-  if (isExpired) {
-    throw new BadRequestError('The code you entered has expired. Please request a new code.')
-  }
-  await db.query('DELETE FROM email_verification_codes WHERE email = $1 AND verification_code = $2', [email, verification_code])
-  return res.status(200).json({ status: 'ok' })
-})
 
 export const createAccountHandler = catchAsync(async function (req, res, next) {
   const { given_name, email, password } = req.body
--- api/src/routes/auth.route.ts
+++ api/src/routes/auth.route.ts
@@ -1,10 +1,7 @@
 import { Router } from "express";
 import { 
   createAccountHandler, 
-  emailAvailableHandler, 
   loginHandler, 
-  sendEmailConfirmationEmailHandler, 
-  verfyEmailConfirmationCodeHandler 
 } from "../controllers/auth.controller.js";
 
 const router = Router()
@@ -12,11 +9,5 @@ const router = Router()
 router.post('/signup', createAccountHandler)
 
 router.post('/login', loginHandler)
-
-router.post('/send_confirmation_email', sendEmailConfirmationEmailHandler)
-
-router.post('/verify_email_confirmation_code', verfyEmailConfirmationCodeHandler)
-
-router.get('/email_available', emailAvailableHandler)
 
 export default router
